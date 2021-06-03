e6a2c0a360325500d34953f2df9fd39e2e5fd30b
Benoit Pimpaud <pimpaudben@gmail.com>
2021-06-03 08:46:12 +0200

    [develop] manage scraping error

--- cookbook/main.py
+++ cookbook/main.py
@@ -2,6 +2,16 @@ from cookbook.src.scraping import Scraper
 from cookbook.src.io import write_file, write_folder
 from time import sleep
 
+
+def build_receipe(scraper, receipe, cat_foldername):
+    receipe_filename = receipe.get('caption').lower().replace(" ", "-").replace("/", "")
+    markdown_content = ""
+    markdown_content = markdown_content + f"# {receipe.get('caption')}"
+    receipe_content = scraper.parse_receipe(receipe)
+    markdown_content = markdown_content + f"\n{receipe_content.get('name')}"
+    markdown_content = markdown_content + f"\n![]({receipe_content.get('img')})"
+    write_file(receipe_filename, markdown_content, path=f"docs/{cat_foldername}")
+
 if __name__ == "__main__":
     BASE_URL = "https://cuisine.journaldesfemmes.fr/toutes-les-recettes/"
     scraper = Scraper(BASE_URL)
@@ -12,11 +22,11 @@ if __name__ == "__main__":
         write_folder(cat_foldername)
         print(cat_foldername)
         for receipe in receipes:
-            receipe_filename = receipe.get('caption').lower().replace(" ", "-").replace("/", "")
-            markdown_content = ""
-            markdown_content = markdown_content + f"# {receipe.get('caption')}"
-            receipe_content = scraper.parse_receipe(receipe)
-            markdown_content = markdown_content + f"\n{receipe_content.get('name')}"
-            markdown_content = markdown_content + f"\n![]({receipe_content.get('img')})"
-            write_file(receipe_filename, markdown_content, path=f"docs/{cat_foldername}")
-            sleep(2)
+            try:
+                build_receipe(scraper, receipe, cat_foldername)
+            except:
+                sleep(30)
+                try:
+                    build_receipe(scraper, receipe, cat_foldername)
+                except:
+                    print(f"Fail to parse {receipe.get('url')}")
